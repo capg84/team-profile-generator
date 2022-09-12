aa8ecafa85d867eddb0d2d65a5e726eea1ebdb49
capg84 <cyprian.gomes@live.co.uk>
2022-09-12 18:49:53 +0100

    added jest under script in the package.json and bugfixed in Employee.test.js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,7 +3,7 @@ const Employee = require('../lib/Employee');
 
 // creates employee object 
 test('creates an employee object', () => {
-    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+    const employee = new Employee('Cyprian Gomes', 10, 'cyprian.gomes@live.co.uk');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
@@ -12,21 +12,21 @@ test('creates an employee object', () => {
 
 // gets id from getId() 
 test('gets employee name', () => {
-    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+    const employee = new Employee('Cyprian Gomes', 10, 'cyprian.gomes@live.co.uk');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 // gets emails from getEmail()
 test('gets employee email', () => {
-    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+    const employee = new Employee('Cyprian Gomes', 10, 'cyprian.gomes@live.co.uk');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+    const employee = new Employee('Cyprian Gomes', 10, 'cyprian.gomes@live.co.uk');
 
-    expect(employee.getRole()).toEqual("Employee");
+    expect(employee.getRole()).toEqual('Employee');
 }); 
