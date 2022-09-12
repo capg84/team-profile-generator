de07931ab8d2954d482a01319909cdbe78d84196
capg84 <cyprian.gomes@live.co.uk>
2022-09-12 17:58:15 +0100

    added Employee.test.js to test the employee

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,32 @@
+// using Employee constructor 
+const Employee = require('../lib/Employee');
+
+// creates employee object 
+test('creates an employee object', () => {
+    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// gets id from getId() 
+test('gets employee name', () => {
+    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// gets emails from getEmail()
+test('gets employee email', () => {
+    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const employee = new Employee('Cyprian Gomes', 01, 'cyprian.gomes@live.co.uk');
+
+    expect(employee.getRole()).toEqual("Employee");
+}); 
